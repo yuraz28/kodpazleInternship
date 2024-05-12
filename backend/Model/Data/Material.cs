0c370bedc185d9bffe15806eba6e7d68c6cd653a
Frain <frainworks1@mail.ru>
2024-05-12 22:55:45 +0300

    Material работает, я проверил

---
+++ backend/Model/Data/Material.cs
@@ -0,0 +1,26 @@
+public class Material
+{
+    public int ID { get; set; }
+    public int IdAuthor { get; set; }
+    public string? Name {get; set;}
+    public string? Information { get; set; }
+    public string? UrlImage { get; set; }
+    public List<int>? Rating { get; set; }
+
+    public int? Rate { get; set; }
+    public int? TimeToLearn { get; set; }
+
+    public Material(int id, int idAuthor, string name, string information, string urlImage, List<int> rating)
+    {
+        ID = id; 
+        IdAuthor = idAuthor;
+        Name = name; 
+        Information = information; 
+        UrlImage = urlImage;
+        Rating = rating;
+        Rate = Convert.ToInt32(Rating.Sum() / Rating.Count());
+        TimeToLearn = Convert.ToInt32((Information.Split()).Count());
+    }
+
+    public Material() { }
+}
--- backend/Model/Implementation/TaskRepository.cs
+++ backend/Model/Implementation/TaskRepository.cs
@@ -1,14 +1,16 @@
-public class TaskRepository : ITaskRepository
-{
-    private readonly TaskTrackerContext _context;
 
-    public TaskRepository(TaskTrackerContext context)
-    {
-        _context = context;
-    }
 
-    public TrackerTask GetTaskById(int taskId)
-    {
-        return _context.TrackerTasks.FirstOrDefault(t => t.ID == taskId);
-    }
-}
+// public class TaskRepository : ITaskRepository
+// {
+//     private readonly TaskTrackerContext _context;
+
+//     public TaskRepository(TaskTrackerContext context)
+//     {
+//         _context = context;
+//     }
+
+//     public TrackerTask GetTaskById(int taskId)
+//     {
+//         return _context.TrackerTasks.FirstOrDefault(t => t.ID == taskId);
+//     }
+// }
