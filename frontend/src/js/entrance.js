d96a81be4a0c01ba974ba4c15c2c8648540a2258
timurkaff <timalololohka@gmail.com>
2024-05-15 03:29:47 +0400

    now

--- frontend/src/js/entrance.js
+++ frontend/src/js/entrance.js
@@ -1,18 +1,20 @@
 document.getElementById('entrance').addEventListener('click', function() {
-    var login = document.getElementById('login').value;
+    var name = document.getElementById('login').value;
     var password = document.getElementById('password').value;
 
-    fetch('http://localhost:5143/api/user/auth', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-            login: login,
+    axios.get('http://localhost:5143/api/user/authorization', {
+        params: {
+            name: name,
             password: password
-        }),
+        },
+        headers: {
+            'Accept': '*/*'
+        }
     })
-  .then(response => response.json())
-  .then(data => console.log(data))
-  .catch((error) => console.error('Error:', error));
+  .then(function (response) {
+        console.log(response.data);
+    })
+  .catch(function (error) {
+        console.error('Error:', error);
+    });
 });
