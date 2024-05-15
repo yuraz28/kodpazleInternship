40618089c76059ecdd1e9851e5ec964176ca7118
timurkaff <timalololohka@gmail.com>
2024-05-15 23:14:13 +0400

    fix

--- frontend/src/js/entrance.js
+++ frontend/src/js/entrance.js
@@ -7,25 +7,19 @@ document.getElementById('entrance').addEventListener('click', function() {
         "password": password
     };
 
-    var url = 'http://127.0.0.1:5050/api/user/auth';
+    var url = 'http://192.168.161.33:5050/api/user/auth';
 
     axios.post(url, requestBody)
-    .then(function(response) {
-            if (response.data && response.data.message === 'Пользователь не найден') {
-                alert('Пользователь не найден');
-            } else {
-                console.log(response.data);
-
-                window.location.href = 'Backend.html';
-            }
-        })
-    .catch(function(error) {
+   .then(function(response) {
+        console.log(response.data);
+        window.location.href = 'Backend.html';
+    })
+   .catch(function(error) {
             // Обработка ошибок с кодом статуса 500
             if (error.response && error.response.status === 500) {
                 alert('Произошла внутренняя ошибка сервера.');
             } else {
-                // Обработка других типов ошибок
-                console.error('Error:', error);
+                alert('Пользователь не найден');
             }
         });
 });
