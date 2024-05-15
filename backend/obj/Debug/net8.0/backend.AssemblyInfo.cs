799304aa5627ca3d8a2959c4c40b6638bc1b8b99
Frain <frainworks1@mail.ru>
2024-05-16 02:19:19 +0300

    ok

--- backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70471d587ff0e759d07ea0995b68ca3b63d45a30")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20a3eb6c4b4016f8c56d59489ebd42cf403f0fda")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
