caba8e352978508036f58ef4a1eb13a44fc6e078
Frain <frainworks1@mail.ru>
2024-05-17 10:13:44 +0300

    fix

--- backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31d2a0cb93bb9406607463c994b6dec01ae8c04b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d9881386bee0401aab3c68cde01392602e1b129")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
